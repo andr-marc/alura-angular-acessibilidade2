71f05e5184f9a284244e74ede18d57df4ee4befa
André <andre.marcelino@parnamirim.rn.gov.br>
2023-09-11 11:43:12 -0300

    + diretivas para controlar componente apartir do teclado seguindo padroes do WCAG

---
+++ src/app/shared/directives/focus-back/focus-back.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, OnDestroy, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appFocusBack]',
+})
+export class FocusBackDirective implements OnInit, OnDestroy {
+  private lastFocusedElement: Element;
+
+  ngOnInit(): void {
+    this.lastFocusedElement = document.activeElement;
+  }
+
+  ngOnDestroy(): void {
+    if(this.lastFocusedElement) (this.lastFocusedElement as HTMLElement).focus();
+  }
+}
